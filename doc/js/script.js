a19f7be42ae0ce00e856f1555383e2f6de99adcb
someuser <some@email.com>
2020-01-28 18:44:13 +0300

    langing-page on flexbox

---
+++ doc/js/script.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+	showSlides(slideIndex += 1);
+};
+
+function minusSlide() {
+	showSlides(slideIndex -= 1);
+};
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+};
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("item");
+
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+};
